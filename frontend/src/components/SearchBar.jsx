caacfb44f6017d0077368300fe72802629ada8ae
UTSAV0601 <66869728+UTSAV0601@users.noreply.github.com>
2025-07-24 11:35:31 -0700

    able to retrieve json response of the the stock

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,28 +1,30 @@
 import axios from 'axios';
-import {useState} from 'react';
+import { useState } from 'react';
 
 function SearchBar() {
-    const [symbol, setSymbol] = useState("");
-    const [data, setData] = useState (null);
+  const [symbol, setSymbol] = useState('');
+  const [data, setData] = useState(null);
 
-    const fetchStock = async () => {
+  const fetchStock = async () => {
     try {
       const res = await axios.get(`http://localhost:8000/api/stock/${symbol}`);
       setData(res.data);
     } catch (err) {
-      console.error("Error:", err);
+      console.error('Error:', err);
     }
+  };
+
+  return (
+    <div>
+      <input
+        placeholder="Enter stock symbol"
+        value={symbol}
+        onChange={(e) => setSymbol(e.target.value.toUpperCase())}
+      />
+      <button onClick={fetchStock}>Search</button>
+      <pre>{data ? JSON.stringify(data, null, 2) : 'No data yet'}</pre>
+    </div>
+  );
 }
-};
-return (
-  <div>
-    <input
-      placeholder="Enter stock symbol"
-      value={symbol}
-      onChange={(e) => setSymbol(e.target.value.toUpperCase())}
-    />
-     <button onClick={fetchStock}>Search</button>
-     <pre>{data ? JSON.stringify(data, null, 2) : "No data yet"}</pre>
-  </div>
-);
+
 export default SearchBar;
